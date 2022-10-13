318f05fb5674839ce3982eff6c3082d5338e9bd6
lokesh-dc <lokesh.cdewanand@gmail.com>
2022-10-13 23:30:52 +0530

    mongo URI

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ require("dotenv").config({path: __dirname+"/config.env"})
 const mongoose = require("mongoose");
 
 const dbConnect = ()=>{
-    return mongoose.connect(`mongodb+srv://admin:authorlokesh@bloggingdatabase.bfep84o.mongodb.net/blogger`)
+    return mongoose.connect(`${process.env.MONGO_URI}`)
 
 }
 
