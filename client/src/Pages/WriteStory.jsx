0423dc782a3678629203b31f5d52288589952844
lokesh-dc <lokesh.cdewanand@gmail.com>
2022-11-07 23:26:39 +0530

    Styling: Write Story Psge

--- client/src/Pages/WriteStory.jsx
+++ client/src/Pages/WriteStory.jsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Input, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Grid, Input, Text } from "@chakra-ui/react";
 import Sidebar from "../Components/Sidebar";
 
 import style from "../Styles/WriteStory.module.css"
@@ -11,8 +11,14 @@ export default function WriteStory() {
             <Sidebar />
             <Box textAlign="left" className={style.writeStory} >
                 <Text fontSize="3rem">Write your Story</Text>
-                <form>
-                    <Input type="text" placeholder="title"  />
+                <form >
+                    <input type="text" placeholder="title"  />
+                    <input type="text" placeholder="Summary"  />
+                    <textarea type="text" placeholder="Tell your Story..." rows="20" />
+                    <Flex gap="10px" >
+                        <Button className="secondary_button" colorScheme="transparent" color="black" borderRadius="20px">Clear</Button>
+                        <Button colorScheme="transparent" >Publish</Button>
+                    </Flex>
                 </form>
             </Box>
         </Grid>
